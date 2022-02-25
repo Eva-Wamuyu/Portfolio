14e41c9d954fce2ba45c4b7c2d38388dd7c2d119
Eva <rutheve.eva@gmail.com>
2022-02-25 18:43:40 +0300

    st:body change white to another shade of it

--- Js/scripting.js
+++ Js/scripting.js
@@ -3,11 +3,13 @@ let divVar = document.getElementById("intro");
 let divVar2 = document.getElementById("bio");
 let divVar3 = document.getElementById("footer");
 
+
 const alterMode = function (){
     bodyVar.classList.toggle("lightBody");
     divVar.classList.toggle("lightDivs");
     divVar2.classList.toggle("lightDivs");
     divVar3.classList.toggle("lightDivs");
+    
 }
 
 
