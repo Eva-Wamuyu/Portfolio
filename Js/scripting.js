86599b55a70c95e73c405cb57628a326f26515a4
Eva <rutheve.eva@gmail.com>
2022-02-25 16:30:28 +0300

    style: add light mode classes

---
+++ Js/scripting.js
@@ -0,0 +1,11 @@
+
+let darkOrLight = document.getElementById("darkOrLight");
+let darkOrLight2 = document.getElementById("darkOrLight2");
+
+
+
+darkOrLight2.addEventListener("click",alterMode);
+
+const alterMode = function(){
+    
+}
